3e8b223c1ff9177f732fb09757c79d9f77c2c5dc
jugosack <jugoslav_ackoski@yahoo.com>
2023-03-29 12:28:20 +0200

    Add insertions to data.sql file.

--- data.sql
+++ data.sql
@@ -39,3 +39,5 @@ VALUES ('Blossom', '1998-10-13',3, TRUE, 17.0);
 
 INSERT INTO "animals" (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Ditto', '2022-05-14',4, TRUE, 22.0);
+
+----------------------------------------------------------------------------------
