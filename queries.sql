7c430189c6134bb741f4e7daa0ad59c14211c4ad
jugosack <jugoslav_ackoski@yahoo.com>
2023-03-29 13:07:35 +0200

    Add itransactions to queries.sql file.

--- queries.sql
+++ queries.sql
@@ -15,3 +15,56 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-------------------------------------------------------------------------------
+
+--update the animals table by setting the species column to unspecified
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+--------------------------------------------------------------------------------
+BEGIN;
+--Update the animals table by setting the species column to digimon for all animals that have a name ending in mon
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+--Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT;
+---------------------------------------------------------------------------------
+BEGIN;
+--Inside a transaction delete all records in the animals table, then roll back the transaction.
+DELETE FROM animals;
+
+ROLLBACK;
+---------------------------------------------------------------------------------
+BEGIN;
+--Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+--Create a savepoint for the transaction.
+SAVEPOINT sp1;
+
+--Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg=weight_kg * -1;
+
+--Rollback to the savepoint
+ROLLBACK TO sp1;
+
+--Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg=weight_kg * -1 WHERE weight_kg < 0;
+
+--Commit transaction
+COMMIT;
+-------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
